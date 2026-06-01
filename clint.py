5e3f3b4f42b36c2af9a4574de1a4852fdd399100
Nitin <nitinchouhan9111@gmail.com>
2026-06-01 11:46:45 +0530

    change the name of the file clint.py to client.py

--- clint.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/ask"
-
-user = input("Enter your question: ")
-questions = {"question": user}
-response = requests.post(url, json=questions)
-print("Response:", response.json()["answer"])
